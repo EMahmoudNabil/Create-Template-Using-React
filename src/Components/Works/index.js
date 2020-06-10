dce1e16dbe35f8f588dee8e3ac8e8e115269f42a
EMahmoudNabil <e.mahmoudnabil@gmail.com>
2020-06-10 18:40:21 +0200

    Work Component With Styled Component & Axios & lifeCycle

--- src/Components/Works/index.js
+++ src/Components/Works/index.js
@@ -1,19 +1,47 @@
-import React  from 'react';
+import React, { Component }  from 'react';
+import {WorkSection,WorkTitle,Span,Part,Icon,PartTitle,Line,PartDesc} from './style.js'
+import axios from 'axios';
 
 
 
 
+class Work extends Component {
+   
 
-
-const Work=() => {
-    return (
-      <div className="App ">
-        works!!!!
-    
-       
-      </div>
-    );
+  state={
+      works:[]
   }
+  componentDidMount(){
+   axios.get('api/data.json').then(res => {this.setState({works:res.data.works})})   
+  }
+
+
+  render(){
+
+      const{works}= this.state;
+      const worksList = works.map((workItem) => {
+          return(
+              <Part first={workItem.id} key={workItem.id}>
+              <Icon className={workItem.icon_name}></Icon>
+               <PartTitle>{workItem.title}</PartTitle>
+              <Line/>
+              <PartDesc>{workItem.body}
+              </PartDesc>
+          </Part>
+          )
+      })
+      return (
+   
+        <WorkSection>
+             <div className="container">
+                 <WorkTitle><Span>My</Span> Work</WorkTitle>
+
+                 {worksList}
+            </div>
+        </WorkSection>
+
+        );
+}}
 
 
 
